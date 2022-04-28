95300fb3c9676e56643245b2221572df5bb693de
Ocampo7-8 <hector44monto@gmail.com>
2022-04-28 11:19:38 -0500

    creacion formulario modulo tienda

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 app.add_url_rule(routes["register"], view_func=routes["register_controllers"])
 app.add_url_rule(routes["login"], view_func=routes["login_controllers"])
 #app.add_url_rule(routes["crear"], view_func=routes["crear_controllers"])
-#app.add_url_rule(routes["productos"], view_func=routes["productos_controllers"])
+app.add_url_rule(routes["productos"], view_func=routes["productos_controllers"])
 
