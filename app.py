e4525c6c6608a14acd8ebd422c269aeabb789d8c
andrelugolopez <andrelugolopez@gmail.com>
2022-05-05 07:35:26 -0500

    Consulta tienda

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 app.add_url_rule(routes["register"], view_func=routes["register_controllers"])
 app.add_url_rule(routes["login"], view_func=routes["login_controllers"])
 app.add_url_rule(routes["crear"], view_func=routes["crear_controllers"])
+app.add_url_rule(routes["eliminar"], view_func=routes["eliminar_controllers"])
 app.add_url_rule(routes["productos"], view_func=routes["productos_controllers"])
 app.add_url_rule(routes["productoId"], view_func=routes["productoId_controllers"])
 
--- routes.py
+++ routes.py
@@ -1,12 +1,14 @@
 from controllers import RegisterControllers
 from controllers import LoginControllers
 from controllers import CrearControllers
+from controllers import EliminarControllers
 from controllers import ProductosControllers
 from controllers import ProductoIdControllers
 
 routes = {"register": "/register", "register_controllers":RegisterControllers.as_view("register_api"),
 "login": "/login", "login_controllers":LoginControllers.as_view("login_api"),
 "crear": "/crearproducto", "crear_controllers":CrearControllers.as_view("crearProducto_api"),
+"eliminar": "/eliminarproducto", "eliminar_controllers":EliminarControllers.as_view("eliminarProducto_api"),
 "productos": "/productos", "productos_controllers":ProductosControllers.as_view("productos_api"),
 "productoId": "/productoId", "productoId_controllers":ProductoIdControllers.as_view("productoId_api")
 }
